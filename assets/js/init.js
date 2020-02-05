f0a127bf25ccb89160f2da59651f7010d292987c
David Öman <Daoma90@gmail.com>
2020-02-06 00:54:05 +0100

    Added quantity change functionallity

--- assets/js/init.js
+++ assets/js/init.js
@@ -9,17 +9,17 @@ cartRemoveAll.addEventListener("click", clearCart);
 loadProducts();
 
 function loadProducts() {
-	//get json from serv
-	fetch('./assets/js/products.json')
-		//converting to js object
-		.then((data) => data.json())
-		//drawing products based on previous promise
-		.then((products) => {
-			let productHtml = '';
-			//Submitting each article with data from js object
-			products.forEach(function (item) {
-				//increments html
-				productHtml += `
+  //get json from serv
+  fetch("./assets/js/products.json")
+    //converting to js object
+    .then(data => data.json())
+    //drawing products based on previous promise
+    .then(products => {
+      let productHtml = "";
+      //Submitting each article with data from js object
+      products.forEach(function(item) {
+        //increments html
+        productHtml += `
 				<article class="products__item">
 					<img class="products__item-img" src='${item.imageSrc}' width="230" alt=''>
 					<div class="products__item-info">
@@ -27,49 +27,51 @@ function loadProducts() {
 						<p>${item.price}</p>
 					</div>
 					<div class="products__item-info-bottom">
-			  <p>${item.artNr}</p>
-			  <input type="number" class="products-quantity-input" min="1" value="1">
+            <p>${item.artNr}</p>
+            <input type="number" class="products-quantity-input" min="1" value="1">
 						<a href="#" class="btn" data-item="${item.id}">ADD</a>
 					</div>
 				</article>`;
-				//updating html content and pushing all items to new array
-				productSection.innerHTML = productHtml;
-				allItems.push(item);
-			});
+        //updating html content and pushing all items to new array
+        productSection.innerHTML = productHtml;
+        allItems.push(item);
+      });
 
-			//Adding listener to all add buttons,  sending in ID to determine which product through addtoCart()
-			const btns = document.querySelectorAll(`.btn`);
-			for (let i = 0; i < btns.length; i++) {
-				const btn = btns[i];
-				btn.addEventListener("click", () => {
-					const prodID = event.target.dataset.item;
-					addItemToCart(prodID);
-				});
-			}
-		})
-		.catch((err) => console.log('Error in Fetch: ' + err));
+      //Adding listener to all add buttons,  sending in ID to determine which product through addtoCart()
+      const btns = document.querySelectorAll(`.btn`);
+      for (let i = 0; i < btns.length; i++) {
+        const btn = btns[i];
+        btn.addEventListener("click", e => {
+          const prodID = event.target.dataset.item;
+          addItemToCart(e, prodID);
+        });
+      }
+    })
+    .catch(err => console.log("Error in Fetch: " + err));
 }
 
+function addItemToCart(e, id) {
+  //Pushing selected item to cartItem array (Fortsätt på detta David?)
+  //Next step is to append html in cart based on result below
+  cartItems.push(allItems.find(item => item.id === id));
 
-function addItemToCart(id) {
-	//Pushing selected item to cartItem array (Fortsätt på detta David?)
-	//Next step is to append html in cart based on result below
-	cartItems.push(allItems.find(item => item.id === id));
-	console.log(cartItems);
+  //Find the correct item in the cartItem array
+  const clickedItem = cartItems.find(item => item.id === id);
 
-	//Find the correct item in the cartItem array
-	const clickedItem = allItems.find(item => item.id === id);
+  //Quantity now gets added to the cart array when product gets placed in cart
+  const quantity = e.target.previousSibling.previousSibling.value;
+  clickedItem.quantity = quantity;
 
-	const cartList = document.getElementsByClassName("cart_list_container")[0];
+  //Create the structure of the cart HTML
+  const cartList = document.getElementsByClassName("cart_list_container")[0];
 
-	//Create the structure of the cart HTML
-	const cartItemContainer = document.createElement("article");
-	cartItemContainer.classList.add("cart_product_container");
+  const cartItemContainer = document.createElement("article");
+  cartItemContainer.classList.add("cart_product_container");
 
-	const cartItemLine = document.createElement("hr");
-	cartItemLine.classList.add("cart-line");
+  const cartItemLine = document.createElement("hr");
+  cartItemLine.classList.add("cart-line");
 
-	let itemHTML = `
+  let itemHTML = `
 	<img src="${clickedItem.imageSrc}" class="cart_product_image" width="100px" height="100px">
 	<div class="cart_productinfo_container">
     <h5 class="cart_product_name">${clickedItem.name}</h5>
@@ -80,84 +82,98 @@ function addItemToCart(id) {
   </div>
 	`;
 
-	//Inserting the cart HTML into the cart
-	cartItemContainer.innerHTML = itemHTML;
-	cartList.appendChild(cartItemContainer);
-	cartList.appendChild(cartItemLine);
+  //Inserting the cart HTML into the cart
+  cartItemContainer.innerHTML = itemHTML;
+  cartList.appendChild(cartItemContainer);
+  cartList.appendChild(cartItemLine);
 
-	//Add event listener to the remove button for each item placed in cart.
-	cartItemContainer
-		.getElementsByClassName("cart_product_remove")[0]
-		.addEventListener("click", removeItemFromCart);
+  //Add event listener to the remove button for each item placed in cart.
+  cartItemContainer
+    .getElementsByClassName("cart_product_remove")[0]
+    .addEventListener("click", removeItemFromCart);
+
+  //Event listener to change quantity in cart
+  cartItemContainer
+    .getElementsByClassName("cart_product_quantity")[0]
+    .addEventListener("change", cartItemQuantityChange);
+}
+
+function cartItemQuantityChange(event) {
+  const change = event.target;
+  const artNr = change.parentElement.children[1].innerHTML;
+
+  const product = cartItems.find(item => item.artNr === artNr);
+  const productIndex = cartItems.indexOf(product);
+
+  cartItems[productIndex].quantity = change.value;
 }
 
 function removeItemFromCart(event) {
-	const remove = event.target;
+  const remove = event.target;
 
-	const artNr = remove.parentElement.children[1].innerHTML;
+  const artNr = remove.parentElement.children[1].innerHTML;
   console.log(artNr);
-  
+
   const product = cartItems.find(item => item.artNr === artNr);
-  const productPosition = cartItems.indexOf(product);
+  const productIndex = cartItems.indexOf(product);
   console.log(product);
-  console.log(productPosition);
-  cartItems.splice(productPosition, 1);
+  console.log(productIndex);
+  cartItems.splice(productIndex, 1);
   console.log(cartItems);
 
-	//Removes the product and the product separator line from the HTML
-	remove.parentElement.parentElement.nextSibling.remove();
-	remove.parentElement.parentElement.remove();
+  //Removes the product and the product separator line from the HTML
+  remove.parentElement.parentElement.nextSibling.remove();
+  remove.parentElement.parentElement.remove();
 }
 
 function clearCart() {
-	//Select the container for the cart products and removes all children
-	const cartList = document.getElementsByClassName("cart_list_container")[0];
-	while (cartList.firstChild) {
-		cartList.firstChild.remove();
-	}
+  //Select the container for the cart products and removes all children
+  const cartList = document.getElementsByClassName("cart_list_container")[0];
+  while (cartList.firstChild) {
+    cartList.firstChild.remove();
+  }
 }
 
-	//Add item to local storage 
-	function checkOutAddItemtoStorage(itemsArr) {
-		//Each item in arr
-		let count = 0;
-		itemsArr.forEach(item => {
-			//making object to string (for localstorage to read)
-			const item_serialized = JSON.stringify(item);
-			//adding to storage
-			localStorage.setItem(`item_${count++}`, item_serialized);
-		});
+//Add item to local storage
+function checkOutAddItemtoStorage(itemsArr) {
+  //Each item in arr
+  let count = 0;
+  itemsArr.forEach(item => {
+    //making object to string (for localstorage to read)
+    const item_serialized = JSON.stringify(item);
+    //adding to storage
+    localStorage.setItem(`item_${count++}`, item_serialized);
+  });
 
-		console.log(localStorage);
+  console.log(localStorage);
 
-		getItemsFromStorage();
-	}
+  getItemsFromStorage();
+}
 
-	//Loop through localstorage and gets each item as js object
-	//For each iteration should add html content to page
-	function getItemsFromStorage() {
+//Loop through localstorage and gets each item as js object
+//For each iteration should add html content to page
+function getItemsFromStorage() {
+  for (let i = 0; i < localStorage.length; i++) {
+    const storageItem = JSON.parse(localStorage.getItem(`item_${i}`));
+    populateConfirmedItem(storageItem);
+  }
+  //populateConfirmedItems(storageItems);
+}
 
-		for (let i = 0; i < localStorage.length; i++) {
-			const storageItem = JSON.parse(localStorage.getItem(`item_${i}`));
-			populateConfirmedItem(storageItem);
-		}
-		//populateConfirmedItems(storageItems);
-	}
+function populateConfirmedItem(confirmedItem) {
+  const orderArea = document.querySelector("#confirmed-orders");
+  orderArea.innerHTML += JSON.stringify(confirmedItem);
+}
 
-	function populateConfirmedItem(confirmedItem) {
-		const orderArea = document.querySelector("#confirmed-orders");
-		orderArea.innerHTML += JSON.stringify(confirmedItem);
-	}
+const chkOutBtn = document.querySelector("#checkOut");
+chkOutBtn.addEventListener("click", () => {
+  checkOutAddItemtoStorage(cartItems);
+});
 
-	const chkOutBtn = document.querySelector('#checkOut');
-	chkOutBtn.addEventListener('click', () => {
-		checkOutAddItemtoStorage(cartItems);
-	});
+function openCart() {
+  cart.style.width = "400px";
+}
 
-	function openCart() {
-		cart.style.width = "400px";
-	  }
-
-	function closeCart() {
-		cart.style.width = "0";
-	}
+function closeCart() {
+  cart.style.width = "0";
+}
